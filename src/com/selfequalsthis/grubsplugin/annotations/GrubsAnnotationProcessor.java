6ff7b08dec390f533ebc2bede6f03962cec8bb8d
Eitan Konigsburg <konigsburg@nytimes.com>
2012-09-05 12:22:44 -0400

    it worked! now to tweak

--- src/com/selfequalsthis/grubsplugin/annotations/GrubsAnnotationProcessor.java
+++ src/com/selfequalsthis/grubsplugin/annotations/GrubsAnnotationProcessor.java
@@ -1,11 +1,10 @@
 package com.selfequalsthis.grubsplugin.annotations;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
@@ -13,6 +12,8 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 
 import com.selfequalsthis.grubsplugin.GrubsCommandHandler;
 
@@ -20,33 +21,29 @@ import com.selfequalsthis.grubsplugin.GrubsCommandHandler;
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 
 public class GrubsAnnotationProcessor extends AbstractProcessor {
-	
-	private File outputFile = new File("commands.yml");
-	
+
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		
+
 		for (Element element : roundEnv.getElementsAnnotatedWith(GrubsCommandHandler.class)) {
 			if (element.getKind() == ElementKind.METHOD) {
 				GrubsCommandHandler handler = element.getAnnotation(GrubsCommandHandler.class);
 				String command = handler.command();
-				
+
+				Filer filer = this.processingEnv.getFiler();
 				try {
-					boolean newFile = !outputFile.exists();
-					BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true));
-					if (newFile) {
-						writer.write("commands:\n");
-					}
-					writer.write("\t" + command + ":\n");
-					writer.write("\t\tusage: /<command>\n");
-					writer.close();
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+					FileObject fileObj = filer.createResource(StandardLocation.SOURCE_OUTPUT,
+															  "commands",
+															  command + ".part",
+															  element);
+					fileObj.openWriter()
+						.append("    " + command + ":\n")
+						.append("        usage: /<command>\n")
+						.close();
+				} catch (IOException e) { }
 			}
 		}
-		
+
 		return true;
 	}
 
